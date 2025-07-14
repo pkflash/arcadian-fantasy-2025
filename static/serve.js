03176533a3ca92991f81f6b8006deeafda62127a
Andrew Nguyen <anguy424@ucsc.edu>
2025-07-13 22:25:49 -0700

    Implemented member deletion on create page

--- static/serve.js
+++ static/serve.js
@@ -11,15 +11,16 @@ fetch('https://api.start.gg/gql/alpha', {
 
 
 const button1 = document.getElementById('button-team1');
-const container = document.getElementById('new');
 const TOTAL_COST = 1200;
-const remainingMoney = TOTAL_COST;
+let remainingMoney = TOTAL_COST;
 
+// Button1 check only passes on index page
 if (button1){
     button1.addEventListener('click', function() {
+        const container = document.getElementById('new');
         const htmlToInsert = `
                 <div class="leaderboard-container" id="side-bar" style="width: 20%; left: 70%; top: 79.5%; height: 120%; justify-content: center;">
-                <button class="link-button" id="x-button" style="color: #ff0000">X</button>
+                <button class="link-button" id="x-button" style="color: #ff0000">&times</button>
                 <h2>Gang Bangers of All Time</h2>
                 <div class="team-view">
                     <div class="player-card" style="margin: 3%">
@@ -101,9 +102,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const playerButtons = document.querySelectorAll('.cost-card'); // Refers to player buttons on the right
     const teamSlots = document.querySelectorAll('.team-player'); // Refers to team slots on the left
     let budget = document.querySelector('.budget-box .amount'); // Load initial budget amount when content loads
-
     const searchInput = document.getElementById('player-search');
 
+    // The below code should only run on "Create" page
     if (searchInput) {
         searchInput.addEventListener('input', function () {
             const query = searchInput.value.toLowerCase();
@@ -118,31 +119,63 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    playerButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const playerName = button.querySelector('.name').innerText; // 
-            const playerCost = button.querySelector('.cost-badge').innerText;
+    if (playerButtons){
+        playerButtons.forEach(button => {
+        
+                // Function for adding player to roster
+                button.addEventListener('click', () => {
+                    // Get player info from clicked button
+                    const playerName = button.querySelector('.name').innerText;
+                    const playerCost = button.querySelector('.cost-badge').innerText;
 
-            // Convert budget text and player cost text to int, and deduct money accordingly
-            let budgetInt = parseInt(budget.innerText);
-            let costInt = parseInt(playerCost);
-            let newBudget = budgetInt - costInt;
+                    // Convert budget text and player cost text to int, and deduct money accordingly
+                    let costInt = parseInt(playerCost);
 
-            // Don't allow the addition if funds are insufficient
-            if (newBudget < 0){
-                return;
-            }
-            for (let slot of teamSlots) {
-                const status = slot.querySelector('.status');
-                const label = slot.querySelector('.label');
+                    // Don't allow the addition if funds are insufficient
+                    if (remainingMoney - costInt < 0){
+                        return;
+                    }
+                    remainingMoney = remainingMoney - costInt;
 
-                if (status.innerText === "Empty") {
-                    status.innerText = playerCost;
-                    label.innerText = playerName;
-                    budget.innerText = newBudget.toString();
-                    break;
-                }
-            }
-        });    
+                    for (let slot of teamSlots) {
+                        const status = slot.querySelector('.status');
+                        const label = slot.querySelector('.label');
+
+                        if (status.innerText === "Empty") {
+                            status.innerText = playerCost;
+                            label.innerText = playerName;
+                            budget.innerText = remainingMoney.toString();
+                            break;
+                        }
+                    }
+            });    
     });
+    }
+
+    if (teamSlots){
+
+        // Delete current member upon delete button click if slot is not empty 
+        teamSlots.forEach(slot => {
+            slot.addEventListener('click', (e) => {
+                const target = e.target;
+                const status = slot.querySelector('.status');
+                if (target.classList.contains('delete')) {
+                    // Refund credits if slot is filled
+                    if (status.innerText !== "Empty"){
+                        const cost = parseInt(status.innerText);
+                        remainingMoney += cost;
+                        budget.innerText = remainingMoney.toString();
+                    }
+
+                    // Change slot labels to empty state
+                    const label = slot.querySelector('.label');
+                    status.innerText = "Empty";
+                    label.innerText = "Player" + " " + slot.id;
+                    
+                }
+
+            })
+        });
+    }
+    
 });
